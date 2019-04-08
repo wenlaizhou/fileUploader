82bdfe2071e77f5a3999e14595251babb0455dde
zhouwl <zhouwl@missfress.cn>
2019-04-08 15:27:35 +0800

    add file

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # fileUploader
 文件上传下载助手
 
+上传文件方式:
+```
+curl http://ip:port/doupload -F "file=@文件名"
+```
+
 <a href="https://996.icu"><img src="https://img.shields.io/badge/link-996.icu-red.svg" alt="996.icu" /></a>
 
 [![LICENSE](https://img.shields.io/badge/license-Anti%20996-blue.svg)](https://github.com/996icu/996.ICU/blob/master/LICENSE)
---
+++ boot.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+var uploadUi = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <title>Page Not Found</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        * {
+            line-height: 1.2;
+            margin: 0;
+        }
+
+        html {
+            color: #888;
+            display: table;
+            font-family: sans-serif;
+            height: 100%;
+            text-align: center;
+            width: 100%;
+        }
+
+        body {
+            display: table-cell;
+            vertical-align: middle;
+            margin: 2em auto;
+        }
+
+        h1 {
+            color: #555;
+            font-size: 2em;
+            font-weight: 400;
+        }
+
+        p {
+            margin: 0 auto;
+            width: 280px;
+        }
+
+        @media only screen and (max-width: 280px) {
+
+            body,
+            p {
+                width: 95%;
+            }
+
+            h1 {
+                font-size: 1.5em;
+                margin: 0 0 0.3em;
+            }
+
+        }
+
+        .submit {
+            border-radius: 30px;
+            color: #fff;
+            line-height: 1.5;
+            background-color: #d9534f;
+            border-color: #d43f3a;
+            display: inline-block;
+            padding: 6px 12px;
+            margin-bottom: 0;
+            font-size: 16px;
+            font-weight: normal;
+            text-align: center;
+            white-space: nowrap;
+            vertical-align: middle;
+            -ms-touch-action: manipulation;
+            touch-action: manipulation;
+            cursor: pointer;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+            background-image: none;
+
+        }
+
+        .fileBtn {
+            border-radius: 30px;
+            background: #26B99A;
+            border: 1px solid #169F85;
+            color: #fff;
+            line-height: 1.5;
+            display: inline-block;
+            padding: 6px 12px;
+            margin-bottom: 0;
+            font-size: 16px;
+            font-weight: normal;
+            text-align: center;
+            white-space: nowrap;
+            vertical-align: middle;
+            -ms-touch-action: manipulation;
+            touch-action: manipulation;
+            cursor: pointer;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+    </style>
+</head>
+
+<body>
+<h1 style="color: #337ab7">上传文件</h1>
+<br/><br/><br/><br/><br/>
+<form action="/doupload" method="post" enctype="multipart/form-data">
+    <input class="fileBtn" style="width: 300px;" type="file" name="file"/>
+    <br/><br/><br/><br/><br/>
+    <input type="submit" style="width: 300px;" class="submit" value="开始上传"/>
+</form>
+
+</body>
+
+</html>
+`
+
+// SelfPath gets compiled executable file absolute Path
+func SelfPath() string {
+	selfPath, _ := filepath.Abs(os.Args[0])
+	return selfPath
+}
+
+// get absolute filepath, based on built executable file
+func RealPath(fp string) (string, error) {
+	if path.IsAbs(fp) {
+		return fp, nil
+	}
+	wd, err := os.Getwd()
+	return path.Join(wd, fp), err
+}
+
+// SelfDir gets compiled executable file directory
+func SelfDir() string {
+	return filepath.Dir(SelfPath())
+}
+
+var ipReg = regexp.MustCompile("(\\d+\\.\\d+\\.\\d+\\.\\d+)/\\d+")
+
+var selfDir = SelfDir()
+
+func main() {
+	dir := SelfDir()
+	fmt.Println("当前目录: ", dir)
+	port := ":8080"
+	if len(os.Args) > 1 {
+		port = fmt.Sprintf(":%v", os.Args[1])
+	}
+	addrs, _ := net.InterfaceAddrs()
+	netAddr := ""
+	for _, add := range addrs {
+		netAdd := add.String()
+		if ipReg.MatchString(netAdd) && !strings.Contains(netAdd, "127.0.0.") {
+			res := ipReg.FindAllStringSubmatch(netAdd, -1)
+			netAddr = res[0][1]
+			break
+		}
+	}
+	println("启动服务:")
+	println("http://" + netAddr + port)
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		http.ServeFile(writer, request, dir+request.URL.Path)
+	})
+	http.HandleFunc("/upload", func(writer http.ResponseWriter, request *http.Request) {
+		_, _ = writer.Write([]byte(uploadUi))
+		return
+	})
+	http.HandleFunc("/doupload", func(writer http.ResponseWriter, request *http.Request) {
+		fs, fsHeader, err := request.FormFile("file")
+		if err != nil {
+			http.Redirect(writer, request, "/", http.StatusFound)
+			return
+		}
+		fileName := fmt.Sprintf("%s/%s", dir, fsHeader.Filename)
+		fileContent, err := ioutil.ReadAll(fs)
+		if err != nil {
+			http.Redirect(writer, request, "/", http.StatusFound)
+			return
+		}
+		_ = ioutil.WriteFile(fileName, fileContent, os.ModePerm)
+		http.Redirect(writer, request, "/", http.StatusFound)
+		return
+	})
+	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+// 上传文件: curl http://ip:port/doupload -F "file=@文件名"
