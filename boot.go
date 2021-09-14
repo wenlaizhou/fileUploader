04b74c2a3826c3082ea6c7d152e3371b7eb593c9
wenlai <wenlai_zhou@126.com>
2021-09-14 15:37:22 +0800

    update style

--- boot.go
+++ boot.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
@@ -12,123 +13,138 @@ import (
 	"strings"
 )
 
-var uploadUi = `<!DOCTYPE html>
+const uploadUi = `<!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
-    <title>Page Not Found</title>
+    <title>FileUploader</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-        * {
-            line-height: 1.2;
-            margin: 0;
-        }
-
-        html {
-            color: #888;
-            display: table;
-            font-family: sans-serif;
-            height: 100%;
-            text-align: center;
-            width: 100%;
-        }
-
-        body {
-            display: table-cell;
-            vertical-align: middle;
-            margin: 2em auto;
-        }
-
-        h1 {
-            color: #555;
-            font-size: 2em;
-            font-weight: 400;
-        }
-
-        p {
-            margin: 0 auto;
-            width: 280px;
-        }
-
-        @media only screen and (max-width: 280px) {
-
-            body,
-            p {
-                width: 95%;
-            }
-
-            h1 {
-                font-size: 1.5em;
-                margin: 0 0 0.3em;
-            }
-
-        }
-
-        .submit {
-            border-radius: 30px;
-            color: #fff;
-            line-height: 1.5;
-            background-color: #d9534f;
-            border-color: #d43f3a;
-            display: inline-block;
-            padding: 6px 12px;
-            margin-bottom: 0;
-            font-size: 16px;
-            font-weight: normal;
-            text-align: center;
-            white-space: nowrap;
-            vertical-align: middle;
-            -ms-touch-action: manipulation;
-            touch-action: manipulation;
-            cursor: pointer;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-            background-image: none;
-
-        }
-
-        .fileBtn {
-            border-radius: 30px;
-            background: #26B99A;
-            border: 1px solid #169F85;
-            color: #fff;
-            line-height: 1.5;
-            display: inline-block;
-            padding: 6px 12px;
-            margin-bottom: 0;
-            font-size: 16px;
-            font-weight: normal;
-            text-align: center;
-            white-space: nowrap;
-            vertical-align: middle;
-            -ms-touch-action: manipulation;
-            touch-action: manipulation;
-            cursor: pointer;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-        }
-    </style>
+    %s
 </head>
-
 <body>
-<h1 style="color: #337ab7">上传文件</h1>
-<br/><br/><br/><br/><br/>
-<form action="/doupload" method="post" enctype="multipart/form-data">
-    <input class="fileBtn" style="width: 300px;" type="file" name="file"/>
-    <br/><br/><br/><br/><br/>
-    <input type="submit" style="width: 300px;" class="submit" value="开始上传"/>
-</form>
-
+	%s
 </body>
-
 </html>
 `
+
+const uploadHtml = `<h1>Storage Service</h1>
+<br>
+<h2><a style="color: #888; text-decoration: none;" href="https://github.com/wenlaizhou/fileUploader" target="_blank">
+		FileUploader(Common Service)
+	</a>
+</h2>
+<br>
+<h4>
+	<a style="color: #888; text-decoration: none;" href="/">查看目录</a>
+</h4>
+<br>
+<br>
+<form action="/doupload" method="post" enctype="multipart/form-data">
+	<input class="fileBtn" style="width: 300px;" type="file" name="file">
+	<br>
+	<br>
+	<input type="submit" style="width: 300px;" class="submit" value="开始上传">
+</form>`
+
+const style = `
+<style>
+	* {
+		line-height: 1.2;
+		margin: 0;
+	}
+	html {
+		color: #888;
+		display: table;
+		font-family: sans-serif;
+		height: 100%;
+		text-align: center;
+		width: 100%;
+	}
+	body {
+		display: table-cell;
+		vertical-align: middle;
+		margin: 2em auto;
+	}
+	h1 {
+		color: #555;
+		font-size: 2em;
+		font-weight: 400;
+	}
+	p {
+		margin: 0 auto;
+		width: 280px;
+	}
+	@media only screen and (max-width: 280px) {
+		body,
+		p {
+			width: 95%;
+		}
+		h1 {
+			font-size: 1.5em;
+			margin: 0 0 0.3em;
+		}
+	}
+	.submit {
+		border-radius: 30px;
+		color: #fff;
+		line-height: 1.5;
+		background-color: #d9534f;
+		border-color: #d43f3a;
+		display: inline-block;
+		padding: 6px 12px;
+		margin-bottom: 0;
+		font-size: 16px;
+		font-weight: normal;
+		text-align: center;
+		white-space: nowrap;
+		vertical-align: middle;
+		-ms-touch-action: manipulation;
+		touch-action: manipulation;
+		cursor: pointer;
+		-webkit-user-select: none;
+		-moz-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+		background-image: none;
+	}
+	.fileBtn {
+		border-radius: 30px;
+		background: #26B99A;
+		border: 1px solid #169F85;
+		color: #fff;
+		line-height: 1.5;
+		display: inline-block;
+		padding: 6px 12px;
+		margin-bottom: 0;
+		font-size: 16px;
+		font-weight: normal;
+		text-align: center;
+		white-space: nowrap;
+		vertical-align: middle;
+		-ms-touch-action: manipulation;
+		touch-action: manipulation;
+		cursor: pointer;
+		-webkit-user-select: none;
+		-moz-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+	}
+	a {
+		color: black;
+		text-decoration: none;
+	}
+</style>
+`
+
+const script = `<script>
+    var bodyHtml = document.body.innerHTML
+    // document.body.innerHTML = "<h1>this is my new upload</h1>"
+    var uploadLink = document.createElement("a")
+    uploadLink.href = "/upload"
+    let newContent = document.createTextNode("点击上传文件");
+    uploadLink.appendChild(newContent);
+    document.body.insertBefore(uploadLink, document.body.getElementsByTagName("pre")[0])
+</script>`
 
 func SelfPath() string {
 	selfPath, _ := filepath.Abs(os.Args[0])
@@ -188,10 +204,15 @@ func main() {
 			return
 		}
 		http.ServeFile(writer, request, dir+request.URL.Path)
+		url := request.URL.Path
+		if url[len(url)-1] == '/' {
+			io.WriteString(writer, style)
+			io.WriteString(writer, script)
+		}
 	})
 
 	http.HandleFunc("/upload", func(writer http.ResponseWriter, request *http.Request) {
-		_, _ = writer.Write([]byte(uploadUi))
+		_, _ = writer.Write([]byte(fmt.Sprintf(uploadUi, style, uploadHtml)))
 		return
 	})
 
